cbeaaa2b7046b5b79814a677534541f722c15e05
maximehl <maximehendrikseliu@students.berkeley.net>
2018-02-02 10:33:34 -0800

    Initial commit

---
+++ magic-api.js
@@ -0,0 +1,363 @@
+/*list of possible suggestion values
+* https://deckbrew.com/api/ under subheading "Card Filtering"
+* index0: card types
+* index1: subtypes
+*       index1: artifact subtypes
+*       index2: creature subtypes
+*       index3: enchantment subtypes
+*       etc.
+* index2: supertypes
+* index3: name (NO SUGGESTIONS!)
+* index4: oracle rules text (spaces replaced by plus signs)
+* index5: set (three letter code)
+* index6: rarity
+* index7: color(s)
+* index8: multicolor (true or false)
+* index9: format
+* index10: status legality
+*/
+var suggestions = [
+    ["Artifact", "Creature", "Enchantment", "Instant", "Land", "Planeswalker", "Sorcery "], //Card Types
+    [
+        ["Clue", "Equipment", "Vehicle"], //Artifact Subtypes
+        ["Advisor", "Ally", "Angel", "Ape", "Archer", "Artificer", "Assassin", "Avatar",
+            "Barbarian", "Beast", "Berserker", "Bird", "Boar", "Cat", "Centaur", "Cleric",
+            "Construct", "Demon", "Djinn", "Dragon", "Drake", "Druid", "Dryad", "Dwarf",
+            "Efreet", "Elemental", "Elephant", "Elf", "Faerie", "Fish", "Fox", "Fungus",
+            "Gargoyle", "Giant", "Goblin", "Golem", "Griffin", "Horror", "Horse", "Hound",
+            "Human", "Hydra", "Illusion", "Imp", "Insect", "Kavu", "Kithkin", "Knight", "Kor",
+            "Lizard", "Mercenary", "Merfolk", "Minion", "Minotaur", "Monk", "Mutant", "Myr",
+            "Nightmare", "Nomad", "Ogre", "Ooze", "Orc", "Plant", "Rat", "Rebel", "Rhino",
+            "Rogue", "Samurai", "Scarecrow", "Scout", "Serpent", "Shade", "Shaman",
+            "Shapeshifter", "Skeleton", "Sliver", "Snake", "Soldier", "Spellshaper", "Sphinx",
+            "Spider", "Spirit", "Thrull", "Treefolk", "Troll", "Vampire", "Vedalken", "Viashino",
+            "Wall", "Warrior", "Werewolf", "Wizard", "Wolf", "Wurm", "Zombie"], //Creature Subtypes
+        ["Aura", "Cartouche", "Curse"],
+        ["Arcane", "Trap"], //Instant Subtypes
+        ["Plains", "Island", "Swamp", "Mountain", "Forest", "Desert", "Gate", "Lair", "Locus",
+            "Urza's", "Mine", "Power-Plant", "Tower"], //Land Subtypes
+        ["Ajani", "Angrath", "Arlinn", "Ashiok", "Bolas", "Chandra", "Dack", "Daretti",
+            "Domri", "Dovin", "Elspeth", "Freyalise", "Garruk", "Gideon", "Jace", "Karn", "Kaya",
+            "Kiora", "Koth", "Liliana", "Nahiri", "Nissa", "Narset", "Nixilis", "Ral", "Saheeli",
+            "Samut", "Sarkhan", "Sorin", "Tamiyo", "Teferi", "Tezzeret", "Tibalt", "Ugin",
+            "Venser", "Vraska", "Xenagos"], //Planeswalker Subtypes
+        ["Arcane"] //Sorcery Subtypes
+    ], //Card Subtypes: Changes Based On Card Type
+    ["Basic", "Legendary"], //Card Supertypes
+    [], //Name: No Suggestions
+    ["Absorb", "Affinity", "Aftermath", "Amplify", "Annihilator", "Attach", "Aura swap",
+        "Banding", "Bands with other", "Battalion", "Battle cry", "Bestow", "Bloodrush",
+        "Bloodthirst", "Bolster", "Bury", "Bushido", "Buyback", "Cascade", "Champion",
+        "Changeling", "Channel", "Chroma", "Cipher", "Clash", "Conspire", "Convoke",
+        "Counter", "Crew", "Cumulative upkeep", "Cycling", "Dash", "Deathtouch", "Defender",
+        "Delve", "Detain", "Devour", "Domain", "Double strike", "Dredge", "Echo", "Embalm",
+        "Enchant", "Entwine", "Epic", "Equip", "Evoke", "Evolve", "Exalted", "Exert", "Exile",
+        "Exploit", "Extort", "Fabricate", "Fading", "Fateful hour", "Fateseal", "Fear",
+        "Ferocious", "Fight", "First strike", "Flanking", "Flash", "Flashback", "Flip",
+        "Flying", "Forecast", "Fortify", "Frenzy", "Graft", "Grandeur", "Gravestorm", "Haste",
+        "Haunt", "Hellbent", "Heroic", "Hexproof", "Hideaway", "Horsemanship", "Imprint",
+        "Indestructible", "Infect", "Intimidate", "Join forces", "Kicker", "Kinship",
+        "Landfall", "Landhome", "Landwalk", "Level up", "Lifelink",
+        "Living weapon", "Madness", "Manifest", "Meld", "Menace", "Metalcraft", "Miracle",
+        "Modular", "Monstrosity", "Morbid", "Morph", "Multikicker", "Ninjutsu", "Offering",
+        "Overload", "Persist", "Phasing", "Phasing", "Poisonous", "Populate", "Proliferate",
+        "Protection", "Provoke", "Prowess", "Prowl", "Radiance", "Raid", "Rally", "Rampage",
+        "Reach", "Rebound", "Recover", "Regenerate", "Regenerate", "Reinforce", "Renown",
+        "Replicate", "Retrace", "Ripple", "Sacrifice", "Scavenge", "Scry", "Shadow",
+        "Shroud", "Soulbond", "Soulshift", "Splice", "Split second", "Storm", "Sunburst",
+        "Suspend", "Sweep", "Tap/Untap", "Threshold", "Totem armor", "Trample",
+        "Transfigure", "Transform", "Transmute", "Typecycling", "Undying", "Unearth",
+        "Unleash", "Vanishing", "Vigilance", "Wither"], //Rules Text
+    [], //Set (Three Letter Code)
+    [/*"common", "uncommon", "rare", "mythic"*/], //Rarity
+    [/*"white", "blue", "black", "red", "green"*/], //Color(s)
+    [], //Multicolor: True Or False, No Suggestions
+    [/*"Commander", "Legacy", "Modern", "Standard", "Vintage"*/], //Format
+    [] //Legality
+];
+
+$(document).ready(function(){
+    addCollapsibleTriggers($(".collapsible"));
+    buildSetMenu();
+    $("input, select").on("click", function(){
+        event.stopPropagation();
+    });
+});
+
+function addCollapsibleTriggers(objectObject){
+    objectObject.css("height", objectObject.children().first().height());
+    if(objectObject.hasClass("menu")){
+        objectObject.on("click", function(){
+            $(this).toggleClass("shown");
+            $(this).toggleClass("notShown");
+            if($(this).hasClass("shown")){
+                $(this).css("height", getHeightNeeded($(this)));
+            }else{
+                $(this).css("height", $(this).children().first().height());
+            }
+        });
+    }else{
+        objectObject.on("mouseenter", function(){
+            $(this).css("height", getHeightNeeded($(this)));
+            var parentObjects = $(this).parents(".collapsible");
+            if(parentObjects.length>0){
+                parentObjects.css("height", getHeightNeeded(parentObjects));
+            }
+        });
+        objectObject.on("mouseleave", function(){
+            $(this).css("height", $(this).children().first().height());
+            var parentObjects = $(this).parents(".collapsible");
+            if(parentObjects.length>0){
+                parentObjects.css("height", getHeightNeeded(parentObjects));
+            }
+        });
+    }
+}
+
+function getHeightNeeded(objectObject){
+    var clone = $(objectObject).clone().css("height", "auto").appendTo("body");
+    var heightNeeded = clone.height();
+    clone.remove();
+    return heightNeeded;
+}
+
+function buildSetMenu(){
+    var imageLinks = [[1, "-icon.png"], [3, "_SET_SYMBOL.png"], [1, "_SYMBOL_0.png"], [1, "_SET_SYMBOL_12.png"],
+        [2, "_SYMBOL.png"], [1, "_ExpansionSymbol.png"], [6, "_SetSymbol.png"], [61, "_SetIcon.png"], [1, "TEM_SetIcon.png"],
+        [10, "_SetIcon.png"], [1, "LED_SetIcon.png"], [2, "_SetIcon.png"], [1, false],
+        [1, "_SetIcon.png"], [2, false]];
+    var threeLetterCodes = ["xln", "HOU", "AKH", "AER", "KLD", "EMN", "SOI", "OGW", "BFZ", "ORI", "DTK", "FRF",
+        "KTK", "M15", "JOU", "BNG", "THS", "M14", "DGM", "GTC", "RTR", "M13", "AVR", "DKA",
+        "ISD", "M12", "NPH", "MBS", "SOM", "M11", "ROE", "WWK", "ZEN", "M10", "ARB", "CON",
+        "ALA", "EVE", "SHM", "MOR", "LRW", "10E", "FUT", "PLC", "TSP", "CSP", "DIS", "GPT",
+        "RAV", "9ED", "SOK", "BOK", "CHK", "5DN", "DST", "MRD", "8ED", "SCG", "LGN", "ONS",
+        "JUD", "TOR", "ODY", "APC", "7ED", "PLS", "INV", "PCY", "NEM", "MMQ", "UDS", "6ED",
+        "ULG", "USG", "EXO", "STH", "TMP", "WTH", "5ED", "VIS", "MIR", "ALL", "HML", "ICE",
+        "4ED", "FEM", "DRK", "LEG", "3ED", "ATQ", "2ED", "ARN", "LEB", "LEA"];
+    var imageCodes = [];
+    var setNames = ["Ixalan", "Hour of Devastation", "Amonkhet", "Aether Revolt", "Kaladesh",
+        "Eldritch Moon", "Shadows over Innistrad", "Oath of the Gatewatch",
+        "Battle for Zendikar", "Magic Origins", "Dragons of Tarkir", "Fate Reforged",
+        "Khans of Tarkir", "Magic 2015", "Journey into Nyx", "Born of the Gods",
+        "Theros", "Magic 2014", "Dragon's Maze", "Gatecrash", "Return to Ravnica",
+        "Magic 2013", "Avacyn Restored", "Dark Ascension", "Innistrad", "Magic 2012",
+        "New Phyrexia", "Mirrodin Besieged", "Scars of Mirrodin", "Magic 2011",
+        "Rise of the Eldrazi", "Worldwake", "Zendikar", "Magic 2010", "Alara Reborn",
+        "Conflux", "Shards of Alara", "Eventide", "Shadowmoor", "Morningtide", "Lorwyn",
+        "10th Edition", "Future Sight", "Planar Chaos", "Time Spiral", "Coldsnap",
+        "Dissension", "Guildpact", "Ravnica: City of Guilds", "9th Edition",
+        "Saviors of Kamigawa", "Betrayers of Kamigawa", "Champions of Kamigawa",
+        "Fifth Dawn", "Darksteel", "Mirrodin", "8th Edition", "Scourge", "Legions",
+        "Onslaught", "Judgment", "Torment", "Odyssey", "Apocalypse", "7th Edition",
+        "Planeshift", "Invasion", "Prophecy", "Nemesis", "Mercadian Masques",
+        "Urza's Destiny", "6th Edition", "Urza's Legacy", "Urza's Saga", "Exodus",
+        "Stronghold", "Tempest", "Weatherlight", "5th Edition", "Visions", "Mirage",
+        "Alliances", "Homelands", "Ice Age", "4th Edition", "Fallen Empires", "The Dark",
+        "Legends", "Revised Edition", "Antiquities", "Unlimited Edition",
+        "Arabian Nights", "Beta", "Alpha"];
+    var setsPerBlock = [1, 2, 2, 2, 2, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 4, 1, 3, 1, 3, 1, 3, 3, 1, 3, 3, 1,
+        1, 2, 3, 1, 3, 3, 1, 3, 1, 1, 9];
+    var blockNames = ["Ixalan Block", "Amonkhet Block", "Kaladesh Block", "Shadows over Innistrad Block",
+        "Battle For Zendikar Block", "Magic Origins", "Khans of Tarkir Block", "Core Set",
+        "Theros Block", "Core Set", "Return to Ravnica Block", "Core Set", "Innistrad Block",
+        "Core Set", "Scars of Mirrodin Block", "Core Set", "Zendikar Block", "Core Set",
+        "Shards of Alara Block", "Lorwyn-Shadowmoor Block", "Core Set", "Time Spiral Block",
+        "Ice Age Block", "Ravnica Block", "Core Set", "Kamigawa Block", "Mirrodin Block",
+        "Core Set", "Onslaught Block", "Odyssey Block", "Invasion Block", "Core Set",
+        "Invasion Block", "Masques Block", "Core Set", "Urza Block", "Tempest Block",
+        "Core Set", "Mirage Block", "Ice Age Block", "Homelands Block", "Ice Age Block"];
+
+    //build an array of image objects for all the sets
+    var loc = 0;
+    var l1;
+    var l2;
+    for(l1 in imageLinks){
+        for(l2 = 0; l2<imageLinks[l1][0]; l2++){
+            imageCodes[loc] = "";
+            if(imageLinks[l1][1]!==false){
+                if(imageLinks[l1][1][0]!=="_"&&imageLinks[l1][1][0]!=="-"){
+                    imageCodes[loc] = "<img src='https://magic.wizards.com/sites/mtg/files/images/featured/" +
+                        imageLinks[l1][1] + "' class='iconImage'>";
+                }else{
+                    imageCodes[loc] = "<img src='https://magic.wizards.com/sites/mtg/files/images/featured/" +
+                        threeLetterCodes[loc] + imageLinks[l1][1] + "' class='iconImage'>";
+                }
+            }
+            loc++;
+        }
+    }
+    var pageObject;
+    loc = 0;
+    for(l1 in setsPerBlock){
+        pageObject = $("<div id='block" + l1 + "' class='collapsible block canClick collapsible'><p>" +
+            blockNames[l1] + "</p><br></div>");
+        $("#blocksContainer").append(pageObject);
+        addCollapsibleTriggers(pageObject);
+        for(l2 = 0; l2<setsPerBlock[l1]; l2++){
+            pageObject = $("<div class='set canClick' data-letter-code='" + threeLetterCodes[loc] + "'>" +
+                imageCodes[loc] + " " + setNames[loc] + "</div>");
+            $('#block' + l1).append(pageObject);
+            loc++;
+        }
+    }
+    $(".canClick").filter(".block, .set").click(function(){
+        event.stopPropagation();
+        $(this).toggleClass("selected");
+        if($(this).hasClass("block")){
+            var setChildren = $(this).find(".set");
+            if($(this).hasClass("selected")){
+                setChildren.addClass("selected");
+            }else{
+                setChildren.removeClass("selected");
+            }
+        }else{
+            if($(this).parent().find(".set").filter(".selected").length>0){
+                $(this).parent().addClass("selected");
+            }else{
+                $(this).parent().removeClass("selected");
+            }
+        }
+    });
+}
+
+function generateSuggestions(suggestionListIndex){
+    var suggestionList;
+    if(suggestionListIndex===1){
+        var cardType = document.getElementById("data0").value;
+        if(typeof cardType!== "undefined"){
+            var cardSubtypeNum = suggestions[0].indexOf(cardType);
+            suggestionList = suggestions[1][cardSubtypeNum]
+        }
+    }else{
+        suggestionList = suggestions[suggestionListIndex];
+    }
+    var returnList = "";
+    for(var i in suggestionList){
+        returnList+="<option value='" + suggestionList[i] + "'>"
+    }
+    document.getElementById("suggestions").innerHTML = returnList;
+}
+
+var urlSearchTerms = ["type", "subtype", "supertype", "name", "oracle", "set",
+    "rarity", "color", "multicolor", "format", "status"];
+var searchPage = 1;
+var searchURL = 'https://api.deckbrew.com/mtg/cards';
+function searchButtonClicked(){
+    searchURL = 'https://api.deckbrew.com/mtg/cards';
+    var dataObject;
+    var dataReps;
+    for(var i = 0; i<10; i++){
+        dataObject = $("#data" + i);
+        dataReps = dataObject.data("num-options");
+        var n;
+        if(dataReps==="many"){
+            dataObject = document.getElementById("data" + i).value;
+            console.log(dataObject);
+            var searchTerm = "";
+            for(n = 0; n<dataObject.length; n++){
+                if(dataObject[n]===" "){
+                    searchTerm+="+";
+                }else if(dataObject[n]===","){
+                    addURLCueMark(i);
+                    searchURL+=searchTerm.toLowerCase();
+                    searchTerm = "";
+                    if(dataObject[n+1]===" "){
+                        n++;
+                    }
+                }else{
+                    searchTerm+=dataObject[n];
+                }
+            }
+            if(searchTerm!==""){
+                addURLCueMark(i);
+                searchURL+=searchTerm.toLowerCase();
+            }
+        }else if(dataReps==="sets"){
+            dataObject = dataObject.find(".set").filter(".selected");
+            dataObject.each(function(){
+                addURLCueMark(i);
+                searchURL+=$(this).data("letter-code").toLowerCase();
+            });
+        }else if(dataReps==="1"){
+            dataObject = dataObject.value;
+            if(dataObject!==""){ //tagHere: if this doesn't work, try dataObject.length>0
+                addURLCueMark(i);
+                searchURL+=dataObject;
+            }
+        }else{
+            dataObject.find("input:checked").each(function(){
+                console.log($(this).value);
+                addURLCueMark(i);
+                searchURL+=$(this).value;
+            });
+            /*dataReps = parseInt(dataReps);
+            for(n = 0; n<dataReps; n++){
+                dataObject = $("#data" + i + "." + n);
+                if(dataObject.is(":checked")){
+                    addURLCueMark(i);
+                    searchURL+=dataObject.value;
+                }
+            }*/
+        }
+    }
+    searchPage = 1;
+    searchURL+="&page=0";
+    console.log(searchURL);
+    //grabJSON();
+}
+
+function addURLCueMark(iValue){
+    if(searchURL==='https://api.deckbrew.com/mtg/cards'){
+        searchURL += "?";
+    }else{
+        searchURL += "&";
+    }
+    searchURL+=urlSearchTerms[iValue].toLowerCase() + "=";
+}
+
+function clearSearch(){
+    var dataReps;
+    for(var i = 0; i<10; i++){
+        dataObject = $("#data" + i);
+        dataReps = dataObject.data("num-options");
+        if(dataReps==="many" || dataReps==="1"){
+            dataObject.val("");
+        }else if(dataReps==="sets"){
+            dataObject.find(".selected").removeClass("selected");
+        }else {
+            dataObject.find("input:checked").prop("checked", false);
+        }
+    }
+}
+
+function changePage(newPageNum){
+    searchPage = newPageNum;
+    searchURL = searchURL.slice(0, searchURL.length-1);
+    searchURL+=searchPage-1;
+    console.log(searchURL);
+    //grabJSON();
+}
+
+function grabJSON(){
+    $.ajax({
+        url: searchURL,
+        type: 'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function(result){
+            //console.log(result); tagHere remember to remove all console.logs!
+            buildResults(result);
+        },
+        error: function(){
+            alert('Failed!');
+        }
+    });
+}
+
+function buildResults(jsonData){
+    //things!
+}
+
+function buildCard(cardData){
+    //things!
+}
